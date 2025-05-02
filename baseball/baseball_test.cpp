45f751c0638ae15978be8f21de6e6a0aa19b2847
smax <dltmdals826@gmail.com>
2025-05-02 15:57:34 +0900

    28p

--- baseball/baseball_test.cpp
+++ baseball/baseball_test.cpp
@@ -1,9 +1,16 @@
-#include "gmock/gmock.h"
 #include "baseball.cpp"
+#include "gmock/gmock.h"
+using namespace std;
 
 TEST(BaseballGame, TryGameTest) {
 	EXPECT_EQ(1, 1);
 }
+
+TEST(BaseballGame, ThorwExceptionWhenInputLengthIsUnmached) {
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12")), length_error);
+}
+
 
 int main() {
 	::testing::InitGoogleMock();
